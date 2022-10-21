06e594931802569c9958a82e05c7ab9470c945de
Richard Tang <115213709+richardtlp@users.noreply.github.com>
2022-10-21 12:16:41 +0700

    feat: get space details api (#7)
    * fix: update seed data
    
    * feat: implement api to get a space details
    
    * refactor: make status and status return explicit

--- banhmi/src/routes/spaceRoutes.ts
+++ banhmi/src/routes/spaceRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { getAllSpacesInfoHandler } from '@/controllers/spaces';
+import { getAllSpacesInfoHandler, getSpaceDetailsHandler } from '@/controllers/spaces';
 
 const router = Router();
 
 router.get('/', asyncHandler(getAllSpacesInfoHandler));
+
+router.get('/:spaceId', asyncHandler(getSpaceDetailsHandler))
 
 export { router as spaceRouter };
