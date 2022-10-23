4c517a79df3418f6e2be0fc976dd16a9c6ee79d5
vuthienanh <Vu_Thien_Anh@mckinsey.com>
2022-10-23 16:25:51 +0700

    resolve conflict

--- banhmi/src/routes/index.ts
+++ banhmi/src/routes/index.ts
@@ -1,21 +1,15 @@
 import { Router } from 'express';
 
 import { spaceRouter } from '@/routes/spaceRoutes';
-<<<<<<< HEAD
 import { authRouter } from '@/routes/authRoutes';
-=======
 import { friendRequestRouter } from '@/routes/friendRequestRoute';
 import { friendshipRouter } from '@/routes/friendshipRoute';
->>>>>>> develop
 
 const router = Router();
 
 router.use('/spaces', spaceRouter);
-<<<<<<< HEAD
 router.use('/auth', authRouter);
-=======
 router.use('/friends', friendshipRouter);
 router.use('/friends/requests', friendRequestRouter);
->>>>>>> develop
 
 export default router;
