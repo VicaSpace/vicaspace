2d269bf0037cfe4aa6a5b994ebf334c11b36f66f
thienanhvu <115213665+thienanhvu@users.noreply.github.com>
2022-10-24 14:34:50 +0700

    Authentication (#9)
    * create route auth
    
    * implement register flow
    
    * implement get user's salt
    
    * implement login
    
    * implement refresh access token
    
    * implement get user info
    
    * fix db schema
    
    * change _req to req inside auth controller
    
    * add response code 200 to successful request
    
    * import absolute path in main.js
    
    * resolve conflict
    
    * reformat prisma schema
    
    * move authenticate to middleware
    
    * convention fix

--- banhmi/jest.config.js
+++ banhmi/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-const tsconfig = require("./tsconfig.json")
-const moduleNameMapper = require("tsconfig-paths-jest")(tsconfig)
+const tsconfig = require('./tsconfig.json');
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
 
 module.exports = {
   preset: 'ts-jest',
--- banhmi/src/routes/spaceRoutes.ts
+++ banhmi/src/routes/spaceRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { getAllSpacesInfoHandler, getSpaceDetailsHandler } from '@/controllers/spaces';
+import {
+  getAllSpacesInfoHandler,
+  getSpaceDetailsHandler,
+} from '@/controllers/spaces';
 
 const router = Router();
 
 router.get('/', asyncHandler(getAllSpacesInfoHandler));
 
-router.get('/:spaceId', asyncHandler(getSpaceDetailsHandler))
+router.get('/:spaceId', asyncHandler(getSpaceDetailsHandler));
 
 export { router as spaceRouter };
