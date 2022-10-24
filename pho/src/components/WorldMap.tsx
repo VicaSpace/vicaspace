961043d172ac924bb49aa3d4c7be08f6e7c8e8d8
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-10-24 14:31:15 +0700

    feat: integrate world map

---
+++ pho/src/components/WorldMap.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Map } from 'react-map-gl';
+
+const WorldMap: React.FC<{}> = () => {
+  return (
+    <Map
+      initialViewState={{
+        longitude: 105.8067,
+        latitude: 15.9031,
+        zoom: 1,
+      }}
+      style={{ height: '100vh' }}
+      mapStyle="mapbox://styles/mapbox/navigation-day-v1"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    />
+  );
+};
+
+export default WorldMap;
