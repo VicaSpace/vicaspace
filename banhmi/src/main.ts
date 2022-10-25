d469a6f5f49edfa02031e40a82eabe383321f815
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-10-25 15:44:25 +0700

    fix: update config for log and build

--- banhmi/src/main.ts
+++ banhmi/src/main.ts
@@ -24,7 +24,7 @@ const main = async () => {
 
   app.use((err, req, res, next) => {
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'develoment' ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500).send({ error: err.message });
     next();
   });
