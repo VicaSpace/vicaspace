9d2c193c0ec1b56055b8465dc48cc676da8b8bc7
trungnq2k <Trung_Ngo@mckinsey.com>
2022-10-27 17:49:07 +0700

    finalize ChatContainer

---
+++ pho/src/components/ChatArea/ChatContainer/ChatContainer.tsx
@@ -0,0 +1,27 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+
+import ChatDisplay from '@/components/ChatArea/ChatContainer/ChatDisplay/ChatDisplay';
+import ChatEditor from '@/components/ChatArea/ChatContainer/ChatEditor/ChatEditor';
+
+import './ChatContainer.css';
+
+const ChatContainer: React.FC<{ username: string }> = ({ username }) => {
+  return (
+    <Flex w="100%" h="68%" mt="7" justify="center" align="center">
+      <Flex
+        w="90%"
+        h="100%"
+        flexDir="column"
+        justify="center"
+        align="center"
+        backgroundColor="#D2DAFF"
+      >
+        <ChatDisplay username={username} />
+        <ChatEditor username={username} />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ChatContainer;
