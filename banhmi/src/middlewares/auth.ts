e56d66381a3f2fb977fb6e20b2bde037379a49c6
Richard Tang <115213709+richardtlp@users.noreply.github.com>
2022-11-03 21:38:30 +0700

    fix: check if auth header is undefined (#37)
    * fix: check if auth header is undefined
    
    * fix: apply createHttpError
    
    * fix: change 403 to 401

--- banhmi/src/middlewares/auth.ts
+++ banhmi/src/middlewares/auth.ts
@@ -1,9 +1,15 @@
 import { Request } from 'express';
+import createHttpError from 'http-errors';
 
 import { prisma } from '@/db';
 
 const authenticate = async function (req: Request, res, next) {
   const accessToken = req.headers.authorization?.split(' ')[1];
+
+  if (typeof accessToken === 'undefined') {
+    return next(createHttpError(401, 'Unauthorized'));
+  }
+
   const token = await prisma.authToken.findFirst({
     where: {
       accessToken: accessToken,
@@ -11,17 +17,14 @@ const authenticate = async function (req: Request, res, next) {
   });
 
   // check if token is not null
-  if (token === null)
-    return res.status(403).json({
-      error: 'Unauthorized',
-    });
+  if (token === null) {
+    return next(createHttpError(401, 'Unauthorized'));
+  }
 
   // check if accessToken expired
-  if (token.expiredTime < new Date())
-    return res.status(403).json({
-      error: 'Unauthorized',
-    });
-
+  if (token.expiredTime < new Date()) {
+    return next(createHttpError(401, 'Unauthorized'));
+  }
   // get user profile
   const user = await prisma.user.findFirst({
     where: {
