52e31c70a70351b707c523ef3cb41ac8ae835aec
chaule-hub <115213588+chaule-hub@users.noreply.github.com>
2022-11-03 16:10:27 +0700

    Add toolbar (#32)
    * init toolbar
    
    * add toolbar
    
    * integrate toolbar and video
    
    * update toolbar fullscreen
    
    * use icons with react-icon
    
    * convert space to 2 in toolbar.css
    
    * fix typos + change fullscreen element to document.body

--- pho/src/pages/NotFound404.tsx
+++ pho/src/pages/NotFound404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
-import './notfound404.css';
+import './NotFound404.css';
 
 const NotFound404: React.FC<{}> = () => {
   const error = useRouteError();
