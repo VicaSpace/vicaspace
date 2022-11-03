235da6444746fb7455b7f0864043f0368c0c7bb6
Richard Tang <115213709+richardtlp@users.noreply.github.com>
2022-11-03 16:21:16 +0700

    fix: lowercase file name (#34)
    * fix: temporarily change file name
    
    * fix: change filename to lowercase
    
    * fix: update import for merge conflict

--- pho/src/routes/AppRouter.tsx
+++ pho/src/routes/AppRouter.tsx
@@ -3,10 +3,10 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Pomodoro from '@/components/Pomodoro/Pomodoro';
 import VideoContainer from '@/components/VideoContainer/VideoContainer';
-import NotFound404 from '@/pages/NotFound404';
-import SpacePage from '@/pages/Space';
 import AboutPage from '@/pages/about';
 import HomePage from '@/pages/home';
+import NotFound404 from '@/pages/notfound404';
+import SpacePage from '@/pages/space';
 
 /**
  * Handles the routing of SPA
