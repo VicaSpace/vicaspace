1a84bd0bc5ee86b497788ed21baaad62635e1986
chaule-hub <115213588+chaule-hub@users.noreply.github.com>
2022-11-04 11:08:41 +0700

    Integrate pomodoro and video (#36)
    * integrate pomodoro and video
    
    * Rename Space.css to space.css
    
    * clean code

--- pho/src/routes/AppRouter.tsx
+++ pho/src/routes/AppRouter.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import Pomodoro from '@/components/Pomodoro/Pomodoro';
-import VideoContainer from '@/components/VideoContainer/VideoContainer';
 import AboutPage from '@/pages/about';
 import HomePage from '@/pages/home';
 import NotFound404 from '@/pages/notfound404';
@@ -19,13 +17,6 @@ const AppRouter: React.FC = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/spaces/:id" element={<SpacePage />} />
-          <Route
-            path="/pomo"
-            element={
-              <Pomodoro timestamp={1666794382000} serverTime={Date.now()} />
-            }
-          />
-          <Route path="/video" element={<VideoContainer />} />
           <Route path="*" element={<NotFound404 />} />
         </Routes>
       </Router>
