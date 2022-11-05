a523d58f1726ff72037bae15c7639f6cc2c33860
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-11-05 12:26:52 +0700

    style: change to 2 spaces

--- banhmi/src/validators/authValidator.ts
+++ banhmi/src/validators/authValidator.ts
@@ -3,18 +3,15 @@ import createError from 'http-errors';
 import { prisma } from '@/db';
 
 const validateRegistrationRequest = async (username: string) => {
-    const user = await prisma.user.findUnique({
-        where: {
-            username: username,
-        },
-    });
-    if (user != null) {
-        const error = createError(
-            400,
-            `username existed`,
-        );
-        throw error;
-    }
-}
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+  if (user != null) {
+    const error = createError(400, `username existed`);
+    throw error;
+  }
+};
 
 export { validateRegistrationRequest };
