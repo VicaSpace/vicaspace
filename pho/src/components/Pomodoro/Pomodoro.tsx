667fd2317d5d2727b9e7d74ce0afdc5a1a1a27ce
Athen Vu <Vu_Thien_Anh@mckinsey.com>
2022-11-06 01:31:34 +0700

    move default position of pomodoro to the right place

--- pho/src/components/Pomodoro/Pomodoro.tsx
+++ pho/src/components/Pomodoro/Pomodoro.tsx
@@ -1,11 +1,22 @@
 import { Box, Center } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 
 import { usePomodoro } from '@/hooks/usePomodoro';
 
 import sessionIcon from '../Pomodoro/session.png';
 import './Pomodoro.css';
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+};
+
+const pomodoroWidthPixels = 200;
+const toolbarRightPosition = 62;
 
 const Pomodoro: React.FC<{
   timestamp: number;
@@ -27,11 +38,15 @@ const Pomodoro: React.FC<{
     pomodoroDuration,
     longBreakDuration,
   });
+  const [windowDimensions] = useState(getWindowDimensions());
 
   return (
     <Draggable
       handle=".handle"
-      defaultPosition={{ x: 100, y: 100 }}
+      defaultPosition={{
+        x: windowDimensions.width - pomodoroWidthPixels - toolbarRightPosition,
+        y: 100,
+      }}
       grid={[25, 25]}
       scale={1}
     >
