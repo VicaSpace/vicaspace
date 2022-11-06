b9f995485ad8948ba82f108231b8c4039ab6ce31
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-11-06 13:26:26 +0700

    feat: use JOI to validate update user requests

--- banhmi/src/routes/userRoutes.ts
+++ banhmi/src/routes/userRoutes.ts
@@ -2,13 +2,19 @@ import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 
 import {
-  getUserFromSocketIdHandler, partialUpdateUserHandler,
+  getUserFromSocketIdHandler,
+  partialUpdateUserHandler,
 } from '@/controllers/user';
 import authenticate from '@/middlewares/auth';
+import checkUpdateUserRequest from '@/middlewares/schemas/updateUserSchema';
 
 const router = Router();
 
 router.get('/', authenticate, asyncHandler(getUserFromSocketIdHandler));
-router.patch('/', authenticate, asyncHandler(partialUpdateUserHandler));
+router.patch(
+  '/',
+  [authenticate, checkUpdateUserRequest],
+  asyncHandler(partialUpdateUserHandler)
+);
 
 export { router as userRouter };
